08fa05ee3391634d42c19e71a54f0343032786a0
doanduccuong <cuong12c12000@gmail.com>
2022-01-06 17:00:05 +0700

    sd

--- lib/config/images.dart
+++ lib/config/images.dart
@@ -1,3 +1,4 @@
 /*  The route image*/
 class AppImage{
+  static const String imgHomePage="assets/images/img_homepage.svg";
 }
--- lib/ui/intro_screen/intro_screen.dart
+++ lib/ui/intro_screen/intro_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:login_page/config/images.dart';
+
 class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Expanded(
+          child: Container(
+            height: 100,
+            child: SvgPicture.asset(AppImage.imgHomePage,fit: BoxFit.cover,),
+          ),
+        )
+    );
   }
 }
